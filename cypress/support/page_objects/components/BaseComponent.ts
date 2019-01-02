6ecb50cae14d0a05b92fe95e5e19634d348430e5
Olaolu Akinsete <akinsbo@gmail.com>
2019-01-02 15:16:37 +0100

    ts: fixed module declaration for cucumber stpes

--- cypress/support/page_objects/components/BaseComponent.ts
+++ cypress/support/page_objects/components/BaseComponent.ts
@@ -1,6 +1,7 @@
 import { Component } from "featureTypes"
 
 export default function makeBaseComponent(): Component {
+  /** Object list of generic properties all components could have */
   const baseComponent: Component = {}
   return baseComponent
 }
