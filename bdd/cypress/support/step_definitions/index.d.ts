3f8e3df812a9158ad322cc34b0298d16ada46181
Olaolu Akinsete <akinsbo@gmail.com>
2019-01-30 06:38:25 +0100

    fixed cucumber ts error

--- bdd/cypress/support/step_definitions/index.d.ts
+++ bdd/cypress/support/step_definitions/index.d.ts
@@ -1,1 +1,5 @@
-declare module 'cypress-cucumber-preprocessor/steps';
+declare module 'cypress-cucumber-preprocessor/steps'{
+    export const Given: any
+    export const Then: any
+    export const When: any
+};
