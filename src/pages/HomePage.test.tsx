4e3174fefa29e2dbb7bde577c04fc873f65f7501
Olaolu Akinsete <akinsbo@gmail.com>
2019-02-09 11:45:46 +0100

    bg: fixed renderer & config type errors in test file

--- src/pages/HomePage.test.tsx
+++ src/pages/HomePage.test.tsx
@@ -1,22 +1,10 @@
-// import * as React from "react"
-// import renderer from "react-test-renderer"
-// import HomePage from "./HomePage"
+import * as React from "react"
+import * as renderer from "react-test-renderer"
+import HomePage from "./HomePage"
 
-// describe("HomePage", () => {
-//   it("renders correctly", () => {
-//     const tree = renderer.create(<HomePage />).toJSON()
-//     expect(tree).toMatchSnapshot()
-//   })
-// })
-
-/* tslint:disable-next-line */
-import React from "react"
-/* tslint:disable-next-line */
-import Enzyme, { shallow } from "enzyme"
-/* tslint:disable-next-line */
-import Adapter from "enzyme-adapter-react-16"
-import Wow from "./HomePage"
-Enzyme.configure({ adapter: new Adapter() })
-it("shallow renders without crashing", () => {
-  shallow(<Wow />)
+describe("HomePage", () => {
+  it("renders correctly", () => {
+    const tree = renderer.create(<HomePage />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
 })
