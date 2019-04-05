dfd5d73d1bfef3b637a50564d66921351b3aa9bd
Olaolu Akinsete <olaoluakinsete@Olaolus-MacBook-Pro.local>
2019-04-05 16:17:12 +0100

    setup homepage title

--- src/pages/HomePage.test.tsx
+++ src/pages/HomePage.test.tsx
@@ -1,9 +1,12 @@
 
 import * as Enzyme from "enzyme"
 import * as Adapter from "enzyme-adapter-react-16"
+
 import "jest-enzyme"
 import * as React from "react"
+import { HelmetProvider } from "react-helmet-async"
 import * as renderer from "react-test-renderer"
+
 import HomePage from "./HomePage"
 
 Enzyme.configure({
@@ -12,7 +15,11 @@ Enzyme.configure({
 
 describe("HomePage", () => {
   it("renders correctly", () => {
-    const tree = renderer.create(<HomePage />).toJSON()
+    const tree = renderer.create(
+    <HelmetProvider>
+      <HomePage />
+    </HelmetProvider>
+    ).toJSON()
     expect(tree).toMatchSnapshot()
   })
   describe("structure", () => {
@@ -27,27 +34,8 @@ describe("HomePage", () => {
       it("should be present", () => {
         expect(headModifierWrapper).toHaveLength(1)
       })
-      describe("title", () => {
-        let titleWrapper: any
-        beforeEach(() => {
-          titleWrapper = headModifierWrapper.find("title")
-        })
-        it("should contain elements", () => {
-          expect(titleWrapper)
-            .toHaveLength(1)
-        })
-        it("should set homepage", () => {
-          expect(titleWrapper).toHaveText("homepage")
-        })
-      })
-      describe("meta", () => {
-        const metaWrapper = wrapper.find("meta")
-        it("should contain meta element", () => {
-          expect(metaWrapper).toHaveLength(1)
-        })
-        it("should contain props", () => {
-          expect(metaWrapper).toHaveProp("name", "Homepage")
-        })
+      it("should contain props title", () => {
+        expect(headModifierWrapper).toHaveProp("title", "Homepage")
       })
     })
   })
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,7 +5,7 @@ import duyonoStonePng from "../images/duyono-stone.png"
 const HomePage = () => (
   <div>
     <div className="application">
-    <HeadModifier title= {"Homepage"} />
+    <HeadModifier title="Homepage" />
     </div>
     <h1>This is the Home page</h1>
 
